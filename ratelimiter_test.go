8b00fdda564e26abbb642b90a79eb7e1769ea12d
ejuju <juju.sel.mail@gmail.com>
2022-05-30 21:04:26 +0200

    refactor test

--- ratelimiter_test.go
+++ ratelimiter_test.go
@@ -1,58 +1,31 @@
 package ratelimiter
 
 import (
-	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
-
-	"github.com/gorilla/mux"
 )
 
-func TestEndToEnd(t *testing.T) {
-	maxRequests := 10
-
+func TestMaxRequests(t *testing.T) {
 	// init test server with ratelimiter middleware
-	router := mux.NewRouter()
-	limiter := NewDefaultLimiter(maxRequests, time.Second, time.Minute)
-	router.Use(New(limiter)) // register rate limiter middleware
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {})
-	server := http.Server{
-		Addr:              ":8080",
-		Handler:           router,
-		ReadHeaderTimeout: 30 * time.Second,
-		ReadTimeout:       30 * time.Second,
-		WriteTimeout:      30 * time.Second,
-		IdleTimeout:       90 * time.Second,
-		MaxHeaderBytes:    8000,
-	}
+	var router http.Handler = http.NewServeMux()
+	limiter := NewDefaultLimiter(1, time.Second, time.Minute)
+	router = New(limiter)(router) // register rate limiter middleware
 
-	// listen and serve server
-	go func() {
-		err := server.ListenAndServe()
-		if err != nil {
-			t.Error(err)
-		}
-	}()
-	time.Sleep(1 * time.Second) // wait for server to start
+	// send valid request
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	resrec := httptest.NewRecorder()
+	router.ServeHTTP(resrec, req)
 
-	// test client
-	nbReqs := maxRequests + 1
-	reqInterval := 100 * time.Millisecond
+	// send invalid request
+	resrec = httptest.NewRecorder()
+	router.ServeHTTP(resrec, req)
 
-	for i := 0; i < nbReqs; i++ {
-		res, err := http.Get("http://localhost:8080/")
-		if err != nil {
-			t.Error(err)
-			return
-		}
-		if res.StatusCode == http.StatusTooManyRequests {
-			if i+1 != maxRequests {
-				t.Error(fmt.Errorf("should have gotten banned after %v requests, got banned after %v instead", maxRequests, i+1))
-				return
-			}
-		}
-
-		time.Sleep(reqInterval)
+	// expect ban from middlewares
+	want := http.StatusTooManyRequests
+	got := resrec.Result().StatusCode
+	if want != got {
+		t.Fatalf("want status %v but got %v", want, got)
 	}
 }
